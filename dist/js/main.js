61aed4c5381e99a3f752fc8a66b2f2c4893eab88
Chris Arsenault <chrisstanarsenault@gmail.com>
2019-12-10 19:42:34 -0500

    menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -11,6 +11,7 @@ let showMenu = false;
 const toggleMenu = () => {
   if(!showMenu) {
     menuBtn.classList.add('close');
+    menu.classList.add('show');
     menuNav.classList.add('show');
     menuBranding.classList.add('show');
     navItems.forEach(item => item.classList.add('show'));
@@ -19,6 +20,7 @@ const toggleMenu = () => {
     showMenu = true;
   } else {
     menuBtn.classList.remove('close');
+    menu.classList.remove('show');
     menuNav.classList.remove('show');
     menuBranding.classList.remove('show');
     navItems.forEach(item => item.classList.remove('show'));
