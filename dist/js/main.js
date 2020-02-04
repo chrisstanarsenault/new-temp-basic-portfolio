71edeaeafac9120628962c4702bccb9998e32f77
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-02-03 19:30:54 -0500

    added script to flip cards on click rather than mouse hover.  Removed hover animation code in css

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,37 +1,50 @@
 // Select DOM Items
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('.nav-item');
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
+const cardForFlip = document.getElementsByClassName("flip-card-inner");
 
 // Set Initial State Of Menu
 let showMenu = false;
 
+// Set Initial State Of Card Flip Animation
+let isCardFlipped = false;
+
+const flipCard = function() {
+  if (!isCardFlipped) {
+    this.style.transform = "rotateY(180deg)";
+    isCardFlipped = true;
+  } else {
+    this.style.transform = "rotateY(0)";
+    isCardFlipped = false;
+  }
+};
+
 const toggleMenu = () => {
-  if(!showMenu) {
-    menuBtn.classList.add('close');
-    menu.classList.add('show');
-    menuNav.classList.add('show');
-    menuBranding.classList.add('show');
-    navItems.forEach(item => item.classList.add('show'));
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
 
     // Set Menu State
     showMenu = true;
   } else {
-    menuBtn.classList.remove('close');
-    menu.classList.remove('show');
-    menuNav.classList.remove('show');
-    menuBranding.classList.remove('show');
-    navItems.forEach(item => item.classList.remove('show'));
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
 
     // Set Menu State
     showMenu = false;
   }
-
+};
+// console.log(cardBackside);
+menuBtn.addEventListener("click", toggleMenu);
+for (let i = 0; i < cardForFlip.length; i++) {
+  cardForFlip[i].addEventListener("click", flipCard);
 }
-
-menuBtn.addEventListener('click', toggleMenu);
-
-
-
